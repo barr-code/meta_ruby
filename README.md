a1eb27e491f131d93ebddc7e0daa9579f9923750
Victoria Barr <vb329@georgetown.edu>
2014-11-17 13:28:47 +0000

    Refactored my attr_accessor? method so it doesn't call on class_eval

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Meta Programming: Day 1 of Makers Academy's "Five Languages in Five Days"
-We were set the [challenge](https://github.com/makersacademy/course/blob/master/level_up/meta_programming.md) of writing a program that will create methods
+We were set the challenge of writing a program that will create methods
 on the fly, but only if they follow the pattern of "has_octocat?".
 Using :method_missing and :define_method, we create a program
 that more or less writes itself.
--- lib/students.rb
+++ lib/students.rb
@@ -1,15 +1,13 @@
 class Student
 
-	def Student::attr_accessor?(boolean)
-		class_eval { define_method(boolean.to_s,
-			lambda { instance_variable_get('@' + boolean.to_s.chop)})}
-		class_eval {define_method(boolean.to_s.chop+"=", lambda {|value| instance_variable_set('@'+boolean.to_s.chop, value)})}
+	def self.attr_accessor?(boolean)
+		self.send(:define_method, boolean.to_s){ instance_variable_get('@' + boolean.to_s.chop)}
+		self.send(:define_method, boolean.to_s.chop+"="){|value|instance_variable_set('@'+boolean.to_s.chop, value)}
 	end
 
 	attr_reader :badges
 	attr_accessor? :winning?
 	
-
 	def initialize
 		@badges = []
 		@winning = false
@@ -21,10 +19,8 @@ class Student
 		self.winning = true
 	end
 
-	def method_missing(method_name)
+	def method_missing(method_name, *args)
 		return false if /^has_.+?\?/.match(method_name.to_s)
+		super method_name, *args
 	end
-
-	
-
 end
